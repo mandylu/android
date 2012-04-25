e015554a38420aa9ca4dfcc246e8469ef1eb2e9f
liuweili <liuweili@baixing.com>
2012-04-25 10:36:19 +0000

    更新版本号到v1.0.1

--- baixing_quanleimu/build.bat
+++ baixing_quanleimu/build.bat
@@ -1,1 +1,1 @@
-"D:\myworkspace\BuildToolForDiffChannels_UMeng\BuildToolForDiffChannels_UMeng\tools\apache-ant-1.8.2\bin\ant" release 
+"D:\myworkspace\BuildToolForDiffChannels_UMeng\build_android_rc\tools\apache-ant-1.8.2\bin\ant" release 
