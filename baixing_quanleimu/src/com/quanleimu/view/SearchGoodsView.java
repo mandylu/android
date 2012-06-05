8b4c42702c38addc50619a7a9f61a6c15e73fb05
xumengyi <xumengyi@baixing.com>
2012-06-05 07:41:01 +0000

    merge item

--- baixing_quanleimu/src/com/quanleimu/view/SearchGoodsView.java
+++ baixing_quanleimu/src/com/quanleimu/view/SearchGoodsView.java
@@ -42,7 +42,7 @@ import com.quanleimu.jsonutil.JsonUtil;
 import com.quanleimu.util.Communication;
 import com.quanleimu.util.Helper;
 import com.quanleimu.util.Util;
-
+import com.quanleimu.adapter.GoodsListAdapter;
 public class SearchGoodsView extends BaseView implements OnScrollListener {
 
 	// 定义控件
@@ -64,7 +64,7 @@ public class SearchGoodsView extends BaseView implements OnScrollListener {
 	public String searchContent = "";
 	public String act_type = "";
 
-	public CommonAdapter commonAdapter = null;
+//	public GoodsListAdapter commonAdapter = null;
 	public List<GoodsDetail> listSearchGoods = new ArrayList<GoodsDetail>();
 	public List<GoodsDetail> listCommonSearchGoods = new ArrayList<GoodsDetail>();
 	public GoodsList goodsList = new GoodsList();
@@ -74,7 +74,7 @@ public class SearchGoodsView extends BaseView implements OnScrollListener {
 	public int startRow = 0;
 
 	public int isFirst = 0;
-	public CommonAdapter adapter;
+	public GoodsListAdapter adapter;
 	public int totalCount = -1;
 	
 	private String backPageName = "";
@@ -237,7 +237,8 @@ public class SearchGoodsView extends BaseView implements OnScrollListener {
 					} else {
 						loadingLayout.setVisibility(View.GONE);
 					}
-					adapter = new CommonAdapter(getContext(), listSearchGoods);
+					adapter = new GoodsListAdapter(getContext(), listSearchGoods);
+					adapter.setHasDelBtn(false);
 					lvSearchResult.setAdapter(adapter);
 					if (pd != null) {
 						pd.dismiss();
@@ -345,205 +346,6 @@ public class SearchGoodsView extends BaseView implements OnScrollListener {
 			}
 
 		}
-	}
-
-	public class CommonAdapter extends BaseAdapter {
-
-		public Context context;
-		public TextView tvDes, tvPrice, tvDateAndAddress;
-		public ImageView ivInfo;
-		public List<GoodsDetail> list = new ArrayList<GoodsDetail>();
-
-		public List<GoodsDetail> getList() {
-			return list;
-		}
-
-		public void setList(List<GoodsDetail> list) {
-			this.list = list;
-		}
-
-		public CommonAdapter() {
-			super();
-			// TODO Auto-generated constructor stub
-		}
-
-		public CommonAdapter(Context context, List<GoodsDetail> list) {
-			super();
-			this.context = context;
-			this.list = list;
-		}
-
-		@Override
-		public int getCount() {
-			// TODO Auto-generated method stub
-			return list.size();
-		}
-
-		@Override
-		public Object getItem(int arg0) {
-			// TODO Auto-generated method stub
-			return arg0;
-		}
-
-		@Override
-		public long getItemId(int position) {
-			// TODO Auto-generated method stub
-			return position;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			LayoutInflater inflater = LayoutInflater.from(context);
-			View v = null;
-//			if (convertView == null) {
-				v = inflater.inflate(R.layout.item_common, null);
-//			} else {
-//				v = (View) convertView;
-//			}
-
-			if (list.size() == 1) {
-				v.setBackgroundResource(R.drawable.btn_s_bg);
-			} else {
-				if (position == 0) {
-					v.setBackgroundResource(R.drawable.btn_top_bg);
-				} else if (position == list.size() - 1) {
-					v.setBackgroundResource(R.drawable.btn_down_bg);
-				} else {
-					v.setBackgroundResource(R.drawable.btn_m_bg);
-				}
-			}
-			tvDes = (TextView) v.findViewById(R.id.tvDes);
-			tvPrice = (TextView) v.findViewById(R.id.tvPrice);
-			tvPrice.setTextColor(Color.RED);
-			tvDateAndAddress = (TextView) v.findViewById(R.id.tvDateAndAddress);
-			tvDateAndAddress.setTextColor(R.color.hui);
-
-			ivInfo = (ImageView) v.findViewById(R.id.ivInfo);
-			BitmapFactory.Options o =  new BitmapFactory.Options();
-            o.inPurgeable = true;
-			Bitmap tmb = BitmapFactory.decodeResource(context.getResources(),R.drawable.moren1, o);
-			Bitmap mb= Helper.toRoundCorner(tmb, 20);
-			tmb.recycle();
-			
-			Bitmap tmb1 = BitmapFactory.decodeResource(context.getResources(),R.drawable.moren, o);
-			Bitmap mb1= Helper.toRoundCorner(tmb1, 20);
-			tmb1.recycle();
-			
-			ivInfo.setImageBitmap(mb);
-
-			
-
-			int type = Util.getWidthByContext(context);
-			RelativeLayout.LayoutParams lp = null;
-			switch (type) {
-			case 240:
-				lp = new RelativeLayout.LayoutParams(45, 45);
-				break;
-			case 320:
-				lp = new RelativeLayout.LayoutParams(60, 60);
-				break;
-			case 480:
-				lp = new RelativeLayout.LayoutParams(90, 90);
-				break;
-			case 540:
-				lp = new RelativeLayout.LayoutParams(100, 100);
-				break;
-			case 640:
-				lp = new RelativeLayout.LayoutParams(120, 120);
-				break;
-			default:
-				lp = new RelativeLayout.LayoutParams(140, 140);
-				break;
-			}
-
-			lp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-			ivInfo.setLayoutParams(lp);
-
-			if (list.get(position).getImageList() == null
-					|| list.get(position).getImageList().equals("")
-					|| list.get(position).getImageList().getResize180() == null
-					|| list.get(position).getImageList().getResize180()
-							.equals("")) {
-				ivInfo.setImageBitmap(mb1);
-			} else {
-				String b = (list.get(position).getImageList().getResize180())
-						.substring(1, (list.get(position).getImageList()
-								.getResize180()).length() - 1);
-				b = Communication.replace(b);
-
-				if (b.contains(",")) {
-					String[] c = b.split(",");
-					if (c[0] == null || c[0].equals("")) {
-						ivInfo.setImageBitmap(mb1);
-					} else {
-						ivInfo.setTag(c[0]);
-						SimpleImageLoader.showImg( ivInfo, c[0],getContext());
-
-						//TODO::check whether this comments cause problem!!!!
-//						if (position <= 3) {
-//							LoadImage.doTask();
-//						}
-
-						// SimpleImageLoader.showImg(ivInfo, c[0],context);
-					}
-				} else {
-					if (b == null || b.equals("")) {
-						ivInfo.setImageBitmap(mb1);
-					} else {
-						ivInfo.setTag(b);
-						SimpleImageLoader.showImg( ivInfo, b, getContext());
-
-//						if (position <= 3) {
-//							LoadImage.doTask();
-//						}
-						// SimpleImageLoader.showImg(ivInfo, b,context);
-					}
-				}
-			}
-
-			// SimpleImageLoader.showImg(ivHead,
-			// list.get(position).UserAvatar,0);
-
-			String price = "";
-			try {
-				price = list.get(position).getMetaValueByKey("价格") + "";
-			} catch (Exception e) {
-				price = "";
-			}
-			if (price.equals("null") || price.equals("")) {
-				tvPrice.setVisibility(View.GONE);
-			} else {
-				tvPrice.setText(price);
-			}
-			tvDes.setText(list.get(position).getValueByKey(GoodsDetail.EDATAKEYS.EDATAKEYS_TITLE));
-
-			String dateV = list.get(position).getValueByKey(GoodsDetail.EDATAKEYS.EDATAKEYS_DATE);
-			if (dateV != null && !dateV.equals("")) {
-				Date date = new Date(Long.parseLong(dateV) * 1000);
-				SimpleDateFormat df = new SimpleDateFormat("MM月dd日",
-						Locale.SIMPLIFIED_CHINESE);
-
-				String areaV = list.get(position).getValueByKey(GoodsDetail.EDATAKEYS.EDATAKEYS_AREANAME);
-				if (areaV != null
-						&& !areaV.equals("")) {
-					tvDateAndAddress.setText(df.format(date) + " "
-							+ areaV);
-				} else {
-					tvDateAndAddress.setText(df.format(date));
-				}
-			} else {
-				String areaV = list.get(position).getValueByKey(GoodsDetail.EDATAKEYS.EDATAKEYS_AREANAME);
-				if (areaV != null
-						&& !areaV.equals("")) {
-					tvDateAndAddress.setText(areaV);
-				} else {
-					tvDateAndAddress.setVisibility(View.GONE);
-				}
-			}
-
-			return v;
-		}
-
 	}
 
 	@Override
