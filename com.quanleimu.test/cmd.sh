aed1b9ec6cfbadce35de21a4317edb7aea77c129
wangweihua <wangweihua@baixing.com>
2012-10-19 02:46:14 +0000

    增加命令行示例

---
+++ com.quanleimu.test/cmd.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+adb shell am instrument -w -e class com.quanleimu.activity.test.MainActivityTest com.quanleimu.activity.test/pl.polidea.instrumentation.PolideaInstrumentationTestRunner
+adb shell am instrument -w -e class com.quanleimu.activity.test.MainActivityTest#testSearchClick com.quanleimu.activity.test/pl.polidea.instrumentation.PolideaInstrumentationTestRunner
