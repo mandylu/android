049077f7d0cd3b5776d313d6a8715545b9edbf4c
daike <daike@baixing.com>
2012-10-24 03:03:57 +0000

    case update

--- com.quanleimu.test/src/com/quanleimu/activity/test/AdListingViewTest.java
+++ com.quanleimu.test/src/com/quanleimu/activity/test/AdListingViewTest.java
@@ -15,15 +15,20 @@ public class AdListingViewTest extends BaixingTestCase {
 	@Test
 	public void testViewListing() throws Exception {
 		
-		// 进入物品交易>手机号码listing页面
+		//点击物品交易>手机号码
 		//检查listing信息不为空
 		//向下浏览30个信息
-		//检查底部提示：正在加载更多，请稍候
-		//向下浏览到第100个信息，进入
-		//检查viewad个数显示100／120
+		//检查底部提示：点击载入下30条
+		//向下拖动
+		//向下浏览30个信息
+		//检查底部提示：点击载入下30条
+	    //向下拖动
+		//检查底部提示：点击载入下30条
+	    //向下拖动
+		//页面展开后，点击最后一个信息进入
 		//从viewad页面返回至listing页面
-		//继续两次返回至一级类目
-	    //检查home title包含百姓网字样
+		//继续三次返回至一级类目
+	    //检查当前页的tab bar上的文字是“浏览信息”
 		//进入车辆买卖>本地下线车
 		//同上手机号码listing页面操作方式
 		
