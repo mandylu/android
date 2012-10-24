4158df7ff414b6515a9f0312be260fc48baf9cf5
daike <daike@baixing.com>
2012-10-24 04:01:51 +0000

    FavoriteTest update

--- com.quanleimu.test/src/com/quanleimu/activity/test/FavoriteTest.java
+++ com.quanleimu.test/src/com/quanleimu/activity/test/FavoriteTest.java
@@ -16,6 +16,7 @@ public class FavoriteTest extends BaixingTestCase {
 	
 	public void testFavoriteAd() throws Exception {
 		
+	   //android2.7.2
 	   //进入类目，房屋租售>租房
 	   //检查列表的title view文字部分包含“租房”
 	   //任选一个信息进入
@@ -34,6 +35,34 @@ public class FavoriteTest extends BaixingTestCase {
 	   //点击返回，返回
 	   //进入我的>收藏信息页面
 	   //检查收藏信息列表，顺序为“标题2”,“标题1”
+		
+	}
+	
+@Test
+	
+	public void testNewFavoriteAd() throws Exception {
+		
+	   //android3.0
+	   //进入类目，房屋租售>租房
+	   //检查列表的title view文字部分包含“租房”
+	   //任选一个信息进入
+	   //检查右上方按钮为收藏前图片（空心五角星）
+	   //点击右上方icon收藏
+	   //检查右上方button文字为收藏后图片（实心五角星）
+	   //记录当前信息的标题，如”标题1“
+	   //当前翻页至下一页
+	   //点击右上方收藏
+	   //翻页至下一页
+	   //点击右上方收藏
+	   //检查右上方button文字为收藏后图片（实心五角星）
+	   //记录当前信息的标题，如”标题2“
+	   //翻至前一页，点击右上方icon取消收藏
+	   //检查右上方按钮为收藏前图片（空心五角星）
+	   //点击返回
+	   //点击返回
+	   //点击返回
+	   //点击我的百姓网>收藏
+	   //检查前两个信息的收藏顺序，顺序应为“标题2”,“标题1”
 		
 	}
 }
