7159d39e4ff3ce3d4b338b28b58be2098bcb38d6
wangweihua <wangweihua@baixing.com>
2012-10-25 02:52:42 +0000

    自动化测试脚本

--- com.quanleimu.test/test.sh
+++ com.quanleimu.test/test.sh
@@ -5,6 +5,8 @@ cd $DIR;
 cd ../;
 svn up;
 
+cp -f com.quanleimu.test/local.properties baixing_quanleimu/local.properties;
+
 adb uninstall com.quanleimu.activity;
 cd baixing_quanleimu/;
 ant clean;
@@ -16,5 +18,9 @@ ant clean;
 ant release;
 adb install -r bin/com.quanleimu.test-release.apk ;
 
+#test adListing,post loop case
 adb shell am instrument -w -e class com.quanleimu.activity.test.KeepLiveTest#runAdListing com.quanleimu.activity.test/pl.polidea.instrumentation.PolideaInstrumentationTestRunner >> testcaserunadlisting.log ;
 adb shell am instrument -w -e class com.quanleimu.activity.test.KeepLiveTest#runPost com.quanleimu.activity.test/pl.polidea.instrumentation.PolideaInstrumentationTestRunner >> testcaserunpost.log ;
+
+#test all case
+#adb shell am instrument -w -e class com.quanleimu.activity.test com.quanleimu.activity.test/pl.polidea.instrumentation.PolideaInstrumentationTestRunner >> testcaseall.log ;
