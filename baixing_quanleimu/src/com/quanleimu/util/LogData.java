e5f57039747faa2ee8d7ae2bacecc7c85b075dd8
xuweiyan <xuweiyan@baixing.com>
2012-10-30 03:37:58 +0000

    reviewer: xu mengyi role:log model

---
+++ baixing_quanleimu/src/com/quanleimu/util/LogData.java
@@ -0,0 +1,46 @@
+package com.quanleimu.util;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+/**
+ * @author xuweiyan@baixing.com
+ */
+public class LogData {
+	
+	private HashMap<String, String> map;
+	
+	//constructor
+	public LogData(HashMap<String, String> map)
+	{
+		this.map = map;
+	}
+	
+	public HashMap<String, String> getMap() {
+		return map;
+	}
+	
+	public LogData append(String key, String value)
+	{
+		this.map.put(key, value);
+		return this;
+	}
+	
+	public void end() {
+		Tracker.getInstance().addLog(this);
+	}
+	
+	public JSONObject toJsonObj() {
+		JSONObject jobj = new JSONObject();
+		try {
+			for (Entry<String,String> entry : map.entrySet()) {
+				jobj.put(entry.getKey(), entry.getValue());
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}		
+		return jobj;
+	}
+}
