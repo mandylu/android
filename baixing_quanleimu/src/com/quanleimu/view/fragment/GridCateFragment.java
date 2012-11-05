2caca45e37b3160dea97d68a563497024739c948
zhongjiawu <zhongjiawu@baixing.com>
2012-11-05 12:39:04 +0000

    #40174 【Android3.0】【新增需求】发布选择一级类目的页面改为列表

--- baixing_quanleimu/src/com/quanleimu/view/fragment/GridCateFragment.java
+++ baixing_quanleimu/src/com/quanleimu/view/fragment/GridCateFragment.java
@@ -1,18 +1,26 @@
 package com.quanleimu.view.fragment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
 
 import com.quanleimu.activity.BaseFragment;
 import com.quanleimu.activity.QuanleimuApplication;
 import com.quanleimu.activity.R;
+import com.quanleimu.adapter.GridAdapter;
 import com.quanleimu.entity.FirstStepCate;
+import com.quanleimu.entity.SecondStepCate;
 import com.quanleimu.util.TrackConfig.TrackMobile.PV;
 import com.quanleimu.util.Tracker;
 import com.quanleimu.widget.CustomizeGridView;
@@ -23,9 +31,13 @@ import com.quanleimu.widget.CustomizeGridView.ItemClickListener;
 //import com.quanleimu.adapter.GridAdapter;
 //import com.quanleimu.adapter.GridAdapter.GridInfo;
 
-public class GridCateFragment extends BaseFragment implements ItemClickListener {
-
-	
+public class GridCateFragment extends BaseFragment implements OnItemClickListener {
+	private static final String []texts 	= {"物品交易", "车辆买卖", "房屋租售", "全职招聘", 
+										   "兼职招聘", "求职简历", "交友活动", "宠物", 
+										   "生活服务", "教育培训"};
+	private static final int []icons 	= {R.drawable.icon_category_wupinjiaoyi, R.drawable.icon_category_car, 		R.drawable.icon_category_house, 	R.drawable.icon_category_quanzhi, 
+										   R.drawable.icon_category_jianzhi,     R.drawable.icon_category_vita, 	R.drawable.icon_category_friend, 	R.drawable.icon_category_pet,
+										   R.drawable.icon_category_service,     R.drawable.icon_category_education};
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,65 +47,24 @@ public class GridCateFragment extends BaseFragment implements ItemClickListener 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.gridcategory, null);
+		View v = inflater.inflate(R.layout.firstcategory, null);
 		v.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
 		
-		int []icons 	= {R.drawable.icon_category_wupinjiaoyi, R.drawable.icon_category_car, 		R.drawable.icon_category_house, 	R.drawable.icon_category_quanzhi, 
-						   R.drawable.icon_category_jianzhi,     R.drawable.icon_category_vita, 	R.drawable.icon_category_friend, 	R.drawable.icon_category_pet,
-						   R.drawable.icon_category_service,     R.drawable.icon_category_education};
-		String []texts 	= {"物品交易", "车辆买卖", "房屋租售", "全职招聘", 
-						   "兼职招聘", "求职简历", "交友活动", "宠物", 
-						   "生活服务", "教育培训"};
-		
-		List<GridInfo> gitems = new ArrayList<GridInfo>();
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		for (int i = 0; i < icons.length; i++)
 		{
-			GridInfo gi = new GridInfo();
-			gi.imgResourceId = icons[i];
-			gi.text = texts[i];
-			gitems.add(gi);
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("tvCategoryName", texts[i]);
+			map.put("ivCategoryImage", icons[i]);			
+			list.add(map);
 		}
-
-//		GridAdapter adapter = new GridAdapter(this.getActivity());
-//		adapter.setList(gitems, 3);
-//		((GridView) v.findViewById(R.id.gridcategory)).setAdapter(adapter);
-//		((GridView) v.findViewById(R.id.gridcategory)).setOnItemClickListener(this);
-		CustomizeGridView grid = (CustomizeGridView) v.findViewById(R.id.gridcategory);
-		grid.setData(gitems, 3);
-		grid.setItemClickListener(this);
+		
+		SimpleAdapter adapter = new SimpleAdapter(getActivity(), list, R.layout.item_category, 
+				new String[]{"tvCategoryName", "ivCategoryImage"}, new int[]{R.id.tvCategoryName, R.id.ivCategoryImage});
+		GridView gridView = (GridView) v.findViewById(R.id.firstcategory);
+		gridView.setAdapter(adapter);
+		gridView.setOnItemClickListener(this);
 		return v;
-	}
-
-	@Override
-	public void onItemClick(GridInfo info, int index) {
-		List<FirstStepCate> allCates = QuanleimuApplication.getApplication()
-				.getListFirst();
-		if (allCates == null)
-			return;
-		if (info == null)
-			return;
-		FirstStepCate selectedCate = null;
-		for (int i = 0; i < allCates.size(); ++i) {
-			String selText = info.text;
-			if (allCates.get(i).name.equals(selText)){
-				selectedCate = allCates.get(i);
-				break;
-			}
-		}
-		
-		if (this.requestCode != INVALID_REQUEST_CODE)
-		{
-			this.finishFragment(this.requestCode, selectedCate);
-		}
-		else
-		{
-			Bundle bundle = new Bundle();
-			bundle.putInt(ARG_COMMON_REQ_CODE, this.requestCode);
-			bundle.putSerializable("cates", selectedCate);
-			bundle.putBoolean("isPost", true);
-			pushFragment(new SecondCateFragment(), bundle);
-		}
-		
 	}
 	
 	@Override
@@ -120,5 +91,36 @@ public class GridCateFragment extends BaseFragment implements ItemClickListener 
 	public void initTab(TabDef tab){
 		tab.m_visible = true;
 		tab.m_tabSelected = ETAB_TYPE.ETAB_TYPE_PUBLISH;
+	}
+
+
+	@Override
+	public void onItemClick(AdapterView<?> adapterView, View view, int index, long arg3) {
+		List<FirstStepCate> allCates = QuanleimuApplication.getApplication()
+				.getListFirst();
+		if (allCates == null || allCates.size() <= index)
+			return;
+		
+		FirstStepCate selectedCate = null;
+		String selText = texts[index];
+		for (int i = 0; i < allCates.size(); ++i) {
+			if (allCates.get(i).name.equals(selText)){
+				selectedCate = allCates.get(i);
+				break;
+			}
+		}
+		
+		if (this.requestCode != INVALID_REQUEST_CODE)
+		{
+			this.finishFragment(this.requestCode, selectedCate);
+		}
+		else
+		{
+			Bundle bundle = new Bundle();
+			bundle.putInt(ARG_COMMON_REQ_CODE, this.requestCode);
+			bundle.putSerializable("cates", selectedCate);
+			bundle.putBoolean("isPost", true);
+			pushFragment(new SecondCateFragment(), bundle);
+		}
 	}
 }
