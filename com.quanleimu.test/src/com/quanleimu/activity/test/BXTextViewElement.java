d5f1cd94049bcbaa4652cf5db0a7a97ee45a63ad
wangweihua <wangweihua@baixing.com>
2012-11-08 06:50:24 +0000

    测试用例修改

---
+++ com.quanleimu.test/src/com/quanleimu/activity/test/BXTextViewElement.java
@@ -0,0 +1,27 @@
+package com.quanleimu.activity.test;
+
+import org.athrun.android.framework.viewelement.TextViewElement;
+
+import android.app.Instrumentation;
+import android.widget.TextView;
+
+public class BXTextViewElement extends TextViewElement {
+	private TextView textView;
+
+	/**
+	 * Constructor of {@code TmtsTextView}.
+	 * 
+	 * @param inst
+	 *            {@code Instrumentation}.
+	 * @param textView
+	 *            {@code TextView}.
+	 */
+	protected BXTextViewElement(Instrumentation inst, TextView textView) {
+		super(inst, textView);
+		this.textView = textView;
+	}
+	
+	public int getInputType() {
+		return textView.getInputType();
+	}
+}
