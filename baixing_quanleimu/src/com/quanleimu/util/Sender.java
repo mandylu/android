c00bfabe5ca0466788ca213e87a135b0220ce749
xuweiyan <xuweiyan@baixing.com>
2012-11-08 02:41:34 +0000

    reviewer: zeng ming role: sender modify

--- baixing_quanleimu/src/com/quanleimu/util/Sender.java
+++ baixing_quanleimu/src/com/quanleimu/util/Sender.java
@@ -3,6 +3,7 @@ package com.quanleimu.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.content.Context;
@@ -16,7 +17,6 @@ public class Sender implements Runnable{
 	private static String apiName = "trackdata";
 	private List<String> queue = null;
 	private static final String SENDER_DIR = "sender_dir";
-	private static final String SENDER_FILE_PREFIX = "bx_sender";//记录文件
 	private static final String SENDER_FILE_SUFFIX = ".log";//记录文件
 	private long dataSize;
 	private Object sendMutex = new Object();
@@ -52,8 +52,7 @@ public class Sender implements Runnable{
 	public void addToQueue(String dataString) {
 		synchronized (queue) {
 			queue.add(dataString);
-			queue.notifyAll();
-		}
+ 		}
 		
 		//Notify send thread to send data.
 		synchronized (sendMutex) {
@@ -86,13 +85,9 @@ public class Sender implements Runnable{
 			newQueue.addAll(queue);
 			queue.clear();
 		}
-		String queueData = "";
-		for (String data : newQueue) {
-			queueData += data + ",";
-		}
-		if (!queueData.equals("")) {
-			queueData = queueData.substring(0, queueData.length()-1);
-			saveListToFile(queueData);
+		
+		if (newQueue.size() > 0) {
+			saveListToFile((new JSONArray(newQueue)).toString());
 			newQueue.clear();
 		}
 	}
@@ -100,7 +95,7 @@ public class Sender implements Runnable{
 	private void saveListToFile(String data)//坑说明：必须注意如果queue中每个元素都存一个文件，按秒来区分文件名，很可能重名，文件覆盖导致数据丢失
 	{
 		if (context != null) {
-			String fileName = SENDER_FILE_PREFIX + System.currentTimeMillis()/1000 + SENDER_FILE_SUFFIX;
+			String fileName = System.currentTimeMillis() + SENDER_FILE_SUFFIX;
 			try {
 				Util.saveDataToFile(context, SENDER_DIR, fileName, data.getBytes());
 			} catch (Exception e) {}			
@@ -125,11 +120,6 @@ public class Sender implements Runnable{
 		return list.get(0);
 	}
 	
-	private String convertListToJson(String list) {
-		return "[" + list + "]";
-	}
-	
-	
 	public static boolean executeSyncPostTask(final String apiName, final String jsonStr) {
 		String url = Communication.getApiUrl(apiName, new ArrayList<String>());
 		url += "&json=";
@@ -150,8 +140,7 @@ public class Sender implements Runnable{
 		}
 	}
 	
-	private boolean sendList(final String list) {//流量统计:统计每次上传成功的字节数
-		String jsonStr = convertListToJson(list);
+	private boolean sendList(final String jsonStr) {//流量统计:统计每次上传成功的字节数
 		Log.d("sendlist",jsonStr);
 		boolean succed = Sender.executeSyncPostTask(apiName, jsonStr);
 		if (succed)
@@ -177,11 +166,11 @@ public class Sender implements Runnable{
 				
 				if (list != null) {//there's memory data, send
 					Log.d("sender", "has memory data");
-					boolean succed = sendList(list);
+					boolean succed = sendList("["+list+"]");
 					Log.d("sender", "after sendList");
 					if (!succed) {
 						Log.d("sender", "saveListToFile");
-						saveListToFile(list);
+						saveListToFile("["+list+"]");
 					}
 				}
 				else	//try load & send persistence file if there is any.
