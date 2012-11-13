7aaef4b5186ed6f6ee246362ef7504e5c842dc61
wangweihua <wangweihua@baixing.com>
2012-11-13 08:36:01 +0000

    测试用例修改

--- com.quanleimu.test/src/com/quanleimu/activity/test/KeepLiveTest.java
+++ com.quanleimu.test/src/com/quanleimu/activity/test/KeepLiveTest.java
@@ -48,7 +48,7 @@ public class KeepLiveTest extends BaixingTestCase {
 		{"TEXT", "联系方式", TEST_DATA_MOBILE},
 		//{"SELECT", "供求", "招聘"},
 		{"TEXT", "具体地点", "测试具体地点测试"},
-		{"TITLE", "补充说明", "招聘职位测试标题"},
+		{"TITLE", "描述", "招聘职位测试标题"},
 		{"TEXT", "工资", "500"},
 		//{"SELECT", "工作地点", "奉贤,全部"},
 		//{"SELECT", "发布人", "公司直招"},
