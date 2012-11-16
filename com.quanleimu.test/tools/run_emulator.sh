c303b13c4d5668efb032c6c35af9a6de70572c30
wangweihua <wangweihua@baixing.com>
2012-11-16 06:30:37 +0000

    脚本调整

--- com.quanleimu.test/tools/run_emulator.sh
+++ com.quanleimu.test/tools/run_emulator.sh
@@ -1,18 +1,19 @@
 #! /bin/sh
 usage(){
-	echo "Usage: $0 emulatorname port"
+	echo "Usage: $0 emulatorname port imgpath"
 	exit 1
 }
-[[ $# -lt 2 ]] && usage
+[[ $# -lt 3 ]] && usage
 emulator="$1"
 port="$2"
+imgpath="$3"
 echo "Check emulator-$port device is connected or wait for one";
 adbState=`adb -s emulator-$port get-state | grep device`;
 if [ "$adbState" = "device" ]; then
     echo "started"
 else
 	echo "Device emulator-$port not found -- connect one to continue..."
-	emulator -avd $emulator -port $port -sdcard logs/emulator/$emulator.img &
+	emulator -avd $emulator -port $port -sdcard imgpath/$emulator.img &
 	adb -s emulator-$port wait-for-device
 	echo "Device emulator-$port connected."
 	sleep 30;
