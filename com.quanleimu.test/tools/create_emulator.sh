b18301802254340d7bfeaf7b60d950eb403db572
wangweihua <wangweihua@baixing.com>
2012-11-19 03:12:29 +0000

    修改脚本

--- com.quanleimu.test/tools/create_emulator.sh
+++ com.quanleimu.test/tools/create_emulator.sh
@@ -17,7 +17,7 @@ if [ "$typeid" = "" ];then
 	exit 1;
 fi
 abi=""
-if [ "$type = "" ];then
+if [ "$type" = "" ];then
 	abi=" --abi armeabi-v7a"
 fi
 (echo "")|android create avd -n $emulator -t $typeid $abi -s WVGA800;
