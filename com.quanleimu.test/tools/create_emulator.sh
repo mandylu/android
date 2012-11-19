63615d210dfacb9e9e104c8cc266dcd05ff59c3f
wangweihua <wangweihua@baixing.com>
2012-11-19 03:59:48 +0000

    修改脚本

--- com.quanleimu.test/tools/create_emulator.sh
+++ com.quanleimu.test/tools/create_emulator.sh
@@ -16,19 +16,20 @@ if [ "$adbState" = "device" ]; then
     echo "$emulator started"
 else
 	android delete avd -n $emulator;  #TODO...run exists avd will crash
+	typeid=`android list |grep "$type"|awk '{print $2}'`;
+	if [ "$typeid" = "" ];then
+		echo "create emulator error by $type";
+		exit 1;
+	fi
+	abi=""
+	if [ "$type" = "android-16" ];then
+		abi=" --abi armeabi-v7a"
+	fi
+	echo "android create avd -n $emulator -t $typeid $abi -s WVGA800";
+	(echo "")|android create avd -n $emulator -t $typeid $abi -s WVGA800;
+	if [ ! -f "$imgpath/$emulator.img" ];
+	then
+		mksdcard 256M "$imgpath/$emulator.img"
+	fi
 fi
-typeid=`android list |grep "$type"|awk '{print $2}'`;
-if [ "$typeid" = "" ];then
-	echo "create emulator error by $type";
-	exit 1;
-fi
-abi=""
-if [ "$type" = "android-16" ];then
-	abi=" --abi armeabi-v7a"
-fi
-echo "android create avd -n $emulator -t $typeid $abi -s WVGA800";
-(echo "")|android create avd -n $emulator -t $typeid $abi -s WVGA800;
-if [ ! -f "$imgpath/$emulator.img" ];
-then
-	mksdcard 256M "$imgpath/$emulator.img"
-fi
+
