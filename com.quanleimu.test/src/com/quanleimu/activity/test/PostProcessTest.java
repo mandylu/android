47c205363b11c31af044d953df9b413b33180c8f
daike <keradaike@gmail.com>
2012-11-20 17:21:33 +0800

    postProcessTest.java update

--- com.quanleimu.test/src/com/quanleimu/activity/test/PostProcessTest.java
+++ com.quanleimu.test/src/com/quanleimu/activity/test/PostProcessTest.java
@@ -94,37 +94,63 @@ public class PostProcessTest extends BaixingTestCase {
 		postSend();
 		//检查发布后未停留在发布页面
 		assertTrue(afterPostSend());
-		//若提示“发布成功”则在已发布列表确认生成
-		//若提示版规错误，则在未审核通过列表确认生成
+		//进入已发布列表		
 		openMyGridByText(MY_LISTING_MYAD_TEXT);
+		//取title
 		ViewElement b1= findElementByText(title);
+		//检查是否取到，取到则删除并返回
 		if (b1 != null) {
 			deleteAdByText(title);
 			return;
 		}
 		goBack();//有无都可以
+		//进入未审核通过页面
 		openMyGridByText(MY_LISTING_MYAD_APPROVE_TEXT);
+		//取发布title
 		ViewElement b2= findElementByText(title);
-		
-		assertTrue(b1 != null || b2 != null);
-		//恢复初始（删除发布信息）
+		//检查是否取到，取到则删除
 		if (b2 != null) {
 			deleteAdByText(title);
 		}
+		//否则未发布成功
+		assertTrue(b1 != null || b2 != null);
 	}
 
    @Test
    public void testQuanzhiProcess() throws Exception {
 	
 	    //android3.0
+	   openTabbar(TAB_ID_POST);
 	    //选择类目“全职求职简历”
+	   openPostFirstCategory(5);
+	   openSecondCategoryByIndex(0);
+	  
 	    //清空所有已填信息
 	    //填入对应test data
+	   String title= postEnterData(postPosTwoShouji);
 	    //全部填完后检查已填内容，确保正确，未被清空
 	    //点击发布按钮
-	    //检查是否发布成功
-	    //若提示“发布成功”则在已发布列表确认生成
-	    //若提示版规错误，则在未审核通过列表确认生成
-	    //删除发布信息
-}
-}
+	   postSend();
+	    //检查发布后未停留在发布页面
+	   assertTrue(afterPostSend());
+	    //进入已发布页面
+	   openMyGridByText(MY_LISTING_MYAD_TEXT);
+	   //取title
+	   ViewElement b1= findElementByText(title);
+	   //检查是否取到title，取到则删除并返回
+	   if (b1 != null){
+		   deleteAdByText(title);
+		   return;
+	   }
+	
+	   //进入未审核通过页面
+	   openMyGridByText(MY_LISTING_MYAD_APPROVE_TEXT);
+	   //取title
+	   ViewElement b2= findElementByText(title);
+	   //检查是否取到，取到则删除
+	   if (b2 != null){
+		  deleteAdByText(title);
+	   }
+	   //若都未取到，则给提示
+	   assertTrue(b1 != null || b2 != null);
+   }
