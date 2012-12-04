1bb335b53686297a3198eb3128d74505362de8d8
wyhw <wyhw@msn.com>
2012-12-04 16:42:11 +0800

    测试用例修改 fix

--- com.quanleimu.test/tools/run_test.sh
+++ com.quanleimu.test/tools/run_test.sh
@@ -8,4 +8,4 @@ emulator="$1"
 func="$2"
 	
 echo "START test $func "
-adb -s $emulator shell am instrument -w -e class $func com.baixing.activity.test/pl.polidea.instrumentation.PolideaInstrumentationTestRunner
+adb -s $emulator shell am instrument -w -e junitXmlOutput class $func com.baixing.activity.test/pl.polidea.instrumentation.PolideaInstrumentationTestRunner
