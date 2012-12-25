28b78f52b6cd2adecc170fb0acc36b87b0a48e84
wyhw <wyhw@msn.com>
2012-12-25 15:23:05 +0800

    summing remove umeng update

--- com.quanleimu.test/tools/remove_umeng.sh
+++ com.quanleimu.test/tools/remove_umeng.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
+
+cp baixing_quanleimu/src/com/quanleimu/activity/QuanleimuMainActivity.java QuanleimuMainActivity.java.bak
 sed 's/UmengUpdateAgent\.update/\/\/UmengUpdateAgent\.update/' < baixing_quanleimu/src/com/quanleimu/activity/QuanleimuMainActivity.java > baixing_quanleimu/src/com/quanleimu/activity/QuanleimuMainActivity.java.2
 mv baixing_quanleimu/src/com/quanleimu/activity/QuanleimuMainActivity.java.2 baixing_quanleimu/src/com/quanleimu/activity/QuanleimuMainActivity.java
