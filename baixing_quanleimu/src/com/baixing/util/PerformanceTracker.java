8de046214eb4ac13b8dbcb5def5f01082e5b58ae
BurizaDo <BurizaDo@163.com>
2013-03-14 16:05:53 +0800

    problem:performance tracker shouldn't been enabled in release solution:comment the implementation. todo:make it more clever

--- baixing_quanleimu/src/com/baixing/util/PerformanceTracker.java
+++ baixing_quanleimu/src/com/baixing/util/PerformanceTracker.java
@@ -34,55 +34,55 @@ public class PerformanceTracker{
 	}
 	
 	private void record(String record){
-		synchronized(getInstance()){
-			records.add(record);
-		}
-		if(records.size() >= MAX_RECORDS){
-			((new Thread(new Runnable(){
-				@Override
-				public void run(){
-					flush();
-				}
-			}))).start();
-		}
+//		synchronized(getInstance()){
+//			records.add(record);
+//		}
+//		if(records.size() >= MAX_RECORDS){
+//			((new Thread(new Runnable(){
+//				@Override
+//				public void run(){
+//					flush();
+//				}
+//			}))).start();
+//		}
 	}
 	
 	public static void flush(){
-		synchronized(getInstance()){
-			FileOutputStream os = null;
-			try {
-				File file = new File(Environment.getExternalStorageDirectory(), "/performance.txt");
-				os = new FileOutputStream(file, true);
-				for(int i = 0; i < getInstance().records.size(); ++ i){
-					try {
-						os.write(getInstance().records.get(i).getBytes());
-						os.write("\n".getBytes());
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch(Exception e){
-				e.printStackTrace();
-			}finally{
-				if(os != null){
-					try {
-						os.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
-			getInstance().records.clear();
-		}
+//		synchronized(getInstance()){
+//			FileOutputStream os = null;
+//			try {
+//				File file = new File(Environment.getExternalStorageDirectory(), "/performance.txt");
+//				os = new FileOutputStream(file, true);
+//				for(int i = 0; i < getInstance().records.size(); ++ i){
+//					try {
+//						os.write(getInstance().records.get(i).getBytes());
+//						os.write("\n".getBytes());
+//					} catch (IOException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+//			} catch (FileNotFoundException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			} catch(Exception e){
+//				e.printStackTrace();
+//			}finally{
+//				if(os != null){
+//					try {
+//						os.close();
+//					} catch (IOException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+//			}
+//			getInstance().records.clear();
+//		}
 	}
 	
 	public static void stamp(PerformEvent.Event event){//, String file, int line, long timestamp){
-		StackTraceElement[] elements = Thread.currentThread().getStackTrace();
-		getInstance().record(event + "," + Thread.currentThread().getStackTrace()[3].getFileName() + "," + Thread.currentThread().getStackTrace()[3].getLineNumber() + "," + String.valueOf(System.currentTimeMillis()));
+//		StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+//		getInstance().record(event + "," + Thread.currentThread().getStackTrace()[3].getFileName() + "," + Thread.currentThread().getStackTrace()[3].getLineNumber() + "," + String.valueOf(System.currentTimeMillis()));
 	}
 }
