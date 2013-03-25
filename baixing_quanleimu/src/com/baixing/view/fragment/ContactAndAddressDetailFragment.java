cba3c31eb39a8f0322c9dd176bae2a445d5b9063
BurizaDo <BurizaDo@163.com>
2013-03-25 17:56:58 +0800

    ADR-12 【发布】将所在地和联系方式挪至input表单之外 - 迭代2

---
+++ baixing_quanleimu/src/com/baixing/view/fragment/ContactAndAddressDetailFragment.java
@@ -0,0 +1,100 @@
+package com.baixing.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.baixing.activity.BaseFragment;
+import com.baixing.activity.BaseFragment.TitleDef;
+import com.baixing.data.GlobalDataManager;
+import com.baixing.entity.BXLocation;
+import com.baixing.util.LocationService;
+import com.baixing.util.ViewUtil;
+import com.quanleimu.activity.R;
+
+public class ContactAndAddressDetailFragment extends BaseFragment{
+	private boolean isContact = false;
+	public static final int MSG_RET_CODE = 0xffff1100;
+	
+	@Override
+	public void initTitle(TitleDef title){
+		title.m_visible = true;
+		title.m_leftActionHint = "返回";
+		title.m_leftActionImage  = R.drawable.icon_close;
+		
+		title.m_rightActionHint = "完成";
+		Bundle bundle = getArguments();
+		if(bundle != null && bundle.containsKey(ARG_COMMON_TITLE)){
+			title.m_title = bundle.getString(ARG_COMMON_TITLE);					
+		}		
+	}
+
+	@Override
+	protected View onInitializeView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		final LinearLayout llEdit = (LinearLayout)inflater.inflate(R.layout.post_contact_addr_edit, null);
+		
+		final Bundle bundle = this.getArguments();
+		if(bundle != null){
+			if(bundle.containsKey("edittype")){
+				if(bundle.getString("edittype").equals("contact")){
+					isContact = true;
+					llEdit.findViewById(R.id.edit_post_location).setVisibility(View.GONE);
+					((TextView)llEdit.findViewById(R.id.contact_edit)).setText(GlobalDataManager.getInstance().getPhoneNumber());
+				}else if(bundle.getString("edittype").equals("address")){
+					isContact = false;
+					llEdit.findViewById(R.id.ll_contact).setVisibility(View.GONE);
+					((TextView)llEdit.findViewById(R.id.postinput)).setText(GlobalDataManager.getInstance().getAddress());
+					llEdit.findViewById(R.id.location).setOnClickListener(new OnClickListener(){
+
+						@Override
+						public void onClick(View v) {
+							// TODO Auto-generated method stub
+							if(bundle.containsKey("location")){
+								BXLocation location = (BXLocation)bundle.getSerializable("location");
+								String address = (location.detailAddress == null || location.detailAddress.equals("")) ? 
+					            		((location.subCityName == null || location.subCityName.equals("")) ?
+												"" 
+												: location.subCityName)
+										: location.detailAddress;
+					            if(address == null || address.length() == 0) return;
+					            if(location.adminArea != null && location.adminArea.length() > 0){
+					            	address = address.replaceFirst(location.adminArea, "");
+					            }
+					            if(location.cityName != null && location.cityName.length() > 0){
+					            	address = address.replaceFirst(location.cityName, "");
+					            }
+								((TextView)llEdit.findViewById(R.id.postinput)).setText(address);
+							}else{
+								llEdit.postDelayed(new Runnable(){
+									@Override
+									public void run(){
+										ViewUtil.showToast(getActivity(), "无法获得当前位置", false);
+									}
+								}, 0);
+							}
+						}
+					});
+				}
+			}
+		}
+		return llEdit;
+	}
+	
+	@Override
+	public void handleRightAction(){
+		if(isContact){
+			GlobalDataManager.getInstance().setPhoneNumber(((TextView)getView().findViewById(R.id.contact_edit)).getText().toString());
+		}else{
+			GlobalDataManager.getInstance().setAddress(((TextView)getView().findViewById(R.id.postinput)).getText().toString());
+		}
+		this.finishFragment();
+	}
+	
+}
