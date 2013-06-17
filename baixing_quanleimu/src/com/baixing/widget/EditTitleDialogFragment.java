3c16d1c63eb1586f10db2f72772f7f8579f5b5fc
BurizaDo <BurizaDo@163.com>
2013-06-17 17:41:36 +0800

    ADR-167 【发布】修改标题的弹出框出现后，标题仍旧随着用户描述的更改而更改 solution:disable at first when edit-dialog appears

--- baixing_quanleimu/src/com/baixing/widget/EditTitleDialogFragment.java
+++ baixing_quanleimu/src/com/baixing/widget/EditTitleDialogFragment.java
@@ -4,9 +4,13 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnShowListener;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.text.Editable;
 import android.text.InputFilter;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -64,8 +68,44 @@ public class EditTitleDialogFragment extends DialogFragment {
 					
 				}
         	});
-        	
+        
         final AlertDialog dialog = builder.create();
+        
+        dialog.setOnShowListener(new OnShowListener(){
+
+			@Override
+			public void onShow(DialogInterface dlg) {
+				// TODO Auto-generated method stub
+				dialog.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
+			}
+        	
+        });
+        
+        etTitle.addTextChangedListener(new TextWatcher(){
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				if(!TextUtils.isEmpty(s) && !s.toString().equals(title)){
+					dialog.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(true);
+				}
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				// TODO Auto-generated method stub
+				
+			}
+        	
+        });
         return dialog;
     }
 
