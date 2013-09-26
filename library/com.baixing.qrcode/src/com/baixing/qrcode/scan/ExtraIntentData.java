6c10db0f2e4a408891ff3e71385421313a594dec
Kingnez <king.fudan@gmail.com>
2013-09-26 14:30:12 +0800

    添加二维码库

---
+++ library/com.baixing.qrcode/src/com/baixing/qrcode/scan/ExtraIntentData.java
@@ -0,0 +1,10 @@
+package com.baixing.qrcode.scan;
+
+public class ExtraIntentData {
+	public static final String EXTRA_COMMON_IS_THIRD_PARTY = "extra.common.isThirdParty";
+	public static final String EXTRA_COMMON_RESULT_CODE = "extra.common.resultCode";
+	public static final String EXTRA_COMMON_DATA = "extra.common.data";
+	public static final String EXTRA_COMMON_INTENT = "extra.common.intent";
+	public static final String EXTRA_COMMON_REQUST_CODE = "extra.image.reqcode";
+	public static final String EXTRA_COMMON_FINISH_CODE = "extra.common.finishCode";
+}
